be49b89144837cfcb9661a9e90082a1c65205cd2
Jean Justine <jeju0004@student.hv.se>
2025-01-08 12:51:12 +0100

    test for filtering not yet tested

--- MyDetectionMethods.py
+++ MyDetectionMethods.py
@@ -15,10 +15,27 @@ class MyDetectionMethods:
         # Apply Gaussian Blur to reduce noise
         blurred = cv2.GaussianBlur(gray, (3, 3), 1.4)
 
-        # Apply Canny edge detection
-        edges = cv2.Canny(blurred, lower_threshold, upper_threshold)
+        thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-        edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+        # Filter using contour area and remove small noise
+        cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        for c in cnts:
+            area = cv2.contourArea(c)
+            if area < 5500:
+                cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
+
+        # Morph close and invert image
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+        edges = 255 - cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+
+
+        # Apply Canny edge detection
+        # edges = cv2.Canny(blurred, lower_threshold, upper_threshold)
+        # edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+
 
         # Find contours
         contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
