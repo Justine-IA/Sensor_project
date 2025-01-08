8aa85d4980ce5d4e3902ccaf9d29409dc75ceae6
Jean Justine <jeju0004@student.hv.se>
2025-01-08 19:43:28 +0100

    everything beside color

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import cv2
 import cv2.aruco as aruco
 from MyDetectionMethods import MyDetectionMethods
 import numpy as np
-import time
 import math
 
 
 def main():
-    last_update_time = time.time()
-    update_interval = 1  # 1 second
+
     
     # Open the default camera
     cap = cv2.VideoCapture(0)
@@ -85,21 +83,24 @@ def main():
                     width = height
                     height = stock
                 
-                if width > 0.8 and height > 2.5 and width <= 1.5 and height <= 3.5:
+                if width > 0.8 and height > 2.3 and width <= 1.5 and height <= 3.5:
                         
                     cv2.drawContours(frame, [contour], 0, (0, 0, 255), 2)
+                #   cv2.circle(frame, centroid, radius=2, color=(0, 0, 255), thickness=-1)
 
                     bottom_left_corner = tuple(box[3])  
 
 
                     text_position = (bottom_left_corner[0], bottom_left_corner[1] + 20)
+                    angle = round(rect[2], 2) 
 
 
-                    text = f"Large"
+                    # Display the angle and classification near the bottom-left corner
+                    text = f"Large" #, Angle:{angle}°
                     cv2.putText(frame, text, text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
 
 
-                elif width > 0.3 and height > 2 and width <0.8 and height<2.4 :
+                elif width > 0.3 and height > 1.7 and width <0.8 and height<2.4 :
                         
                     cv2.drawContours(frame, [contour], 0, (255, 0, 0), 2)
 
@@ -107,9 +108,9 @@ def main():
 
 
                     text_position = (bottom_left_corner[0], bottom_left_corner[1] + 20)
+                    angle = round(rect[2], 2) 
 
-
-                    text = f"Small" # Width:{width:.1f}, Height:{height:.1f}"
+                    text = f"Large" #, Angle:{angle}°
                     cv2.putText(frame, text, text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
 
 
@@ -123,13 +124,14 @@ def main():
 
                     # Offset the text slightly above the bottom-left corner to avoid overlap
                     text_position = (bottom_left_corner[0], bottom_left_corner[1] + 20)
+                    angle = round(rect[2], 1) 
 
 
-                    text = f"Multiple needle superposed Width:{width:.1f}, Height:{height:.1f} "
+                    text = f"Multiple needle superposed  Angle:{angle}° " # , Angle:{angle} Width:{width:.1f}, Height:{height:.1f},
                     cv2.putText(frame, text, text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
 
 
-                elif width>0.2 and height >0.4 and width<1.3 and height <2.4 :
+                elif width>0.2 and height >0.2 and width<1.3 and height <2 :
                     # Draw the circle representing the center of the rectangle in the frame
                     cv2.circle(frame, centroid, radius=2, color=(0, 0, 255), thickness=-1)
                     cv2.drawContours(frame, [box], 0, (255, 0, 255), 2)
@@ -140,11 +142,12 @@ def main():
 
                     # Offset the text slightly above the bottom-left corner to avoid overlap
                     text_position = (bottom_left_corner[0], bottom_left_corner[1] + 20)
+                    angle = round(rect[2], 1) 
 
                     # Draw the rectangle of the object
 
                     # Write the size near the centroid
-                    text = f"BROKER NEEDLE BROKEN NEEDLE"
+                    text = f"BROKER NEEDLE BROKEN NEEDLE "
                     cv2.putText(frame, text, (centroid[0], centroid[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
 
 
